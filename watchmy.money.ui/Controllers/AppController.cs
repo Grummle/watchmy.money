3028979f6142fe79c4050e21bb6a0b3e7866e8e3
Michael Dillon <grummle@gmail.com>
2015-07-10 18:00:02 -0600

    save game

---
+++ watchmy.money.ui/Controllers/AppController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace watchmy.money.Controllers
+{
+    [Authorize]
+    public class AppController : Controller
+    {
+        // GET: App
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
